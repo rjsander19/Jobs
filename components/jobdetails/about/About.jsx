c58197a6f3adf03bc64c271c16966958c03456b6
rjsander <rjsander19@gmail.com>
2023-07-10 11:27:48 -0700

    adds information to job about page

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -7,6 +7,10 @@ const About = ({ info }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.headText}>About the job:</Text>
+
+      <View style={styles.contentBox}>
+        <Text style={styles.contextText}>{info}</Text>
+      </View>
     </View>
   )
 };
