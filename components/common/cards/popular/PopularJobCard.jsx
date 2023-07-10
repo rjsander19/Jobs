2ba87aba45ff9f51309c5b5ce596aa39551b7afe
rjsander <rjsander19@gmail.com>
2023-07-10 11:21:16 -0700

    create and add specifics pages for jobs

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -6,7 +6,8 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
-      onPress={() => handleCardPress(item)}>
+      onPress={() => handleCardPress(item)} 
+    >
 
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
